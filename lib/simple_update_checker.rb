6d8c93e520a66c295b997e4a6ab76922b30fdbc4
dynus <dynus@www6027up.sakura.ne.jp>
2019-01-22 03:40:00 +0900

    Notify if the checking element has been dissapeared

--- lib/simple_update_checker.rb
+++ lib/simple_update_checker.rb
@@ -23,28 +23,31 @@ class SimpleUpdateChecker
   private
 
   def check_update
-    @session.visit @url
-
     content = get_content
     content_digest = digest_content content
 
     last_content_digest = read_last_content_digest
 
-    if content_digest == last_content_digest
+    if content_digest == nil
+      if last_content_digest && last_content_digest.length > 0
+        notify 'Dissappeared!', <<~EOM
+          URL: #{@url}
+        EOM
+        puts "Element #{@selector} lost at #{Time.now}"
+
+        write_last_content_digest('')
+      else
+        puts "No update found at #{Time.new}"
+      end
+    elsif content_digest == last_content_digest
       puts "No update found at #{Time.new}"
     else
       if last_content_digest
-        Mail.deliver do
-          from    email
-          to      email
-          subject 'Updated!'
-          body    <<~EOM
-            #{content}
-            
-            URL: #{@url}
-          EOM
-          charset Encoding::UTF_8
-        end
+        notify 'Updated!', <<~EOM
+          #{content}
+          
+          URL: #{@url}
+        EOM
         puts "Update found at #{Time.new}"
       else
         puts "First check made at #{Time.new}"
@@ -52,17 +55,28 @@ class SimpleUpdateChecker
 
       write_last_content_digest(content_digest)
     end
-  rescue Capybara::ElementNotFound => e
-    puts "Element #{@selector} not found at #{Time.new}"
+  end
+
+  def notify(subject, body)
+    Mail.deliver(
+      from:    @email,
+      to:      @email,
+      subject: subject,
+      body:    body,
+      charset: body.encoding.to_s
+    )
   end
 
   def get_content
+    @session.visit @url
     target_dom = @session.find @selector
     target_dom.text
+  rescue Capybara::ElementNotFound => e
+    nil
   end
 
   def digest_content(content)
-    Digest::MD5.hexdigest content
+    content && Digest::MD5.hexdigest(content)
   end
 
   def read_last_content_digest
